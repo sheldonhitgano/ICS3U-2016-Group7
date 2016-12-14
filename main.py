5fb2ddd32bfe0ff9dffb7835e7833ab8766b24a5
sheldonhitgano <sheldon.hitgano@ocsbstudent.ca>
2016-12-14 14:49:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Created by: Mr. Coxall
+# Created by: Sheldon H
 # Created on: Sep 2016
 # Created for: ICS3U
 # This program is the first file in a multi-scene game template
@@ -20,7 +20,10 @@
 from scene import *
 import ui
 
-from splash_scene import *
+from game_logo_scene import *
+from splash_scene import * 
+
+
 
 
 #  ..use when deploying app for Xcode and the App Store
